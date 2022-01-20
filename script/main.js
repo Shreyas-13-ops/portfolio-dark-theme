701aac8a4a069260faa400ad43aba7c3b94ba05f
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-01-20 13:59:07 +0530

    Update media queries & nav JS

--- script/main.js
+++ script/main.js
@@ -5,3 +5,13 @@ btnMenu.addEventListener('click', () => {
      menu.classList.toggle("active");
     console.log("btn clciked!");
 })
+
+let btnHello = document.querySelector('.btn-hello');
+let contactSection = document.querySelector('.contact');
+
+btnHello.addEventListener('click', () => {
+   let contactUrl =  'https://dark-theme-portfolio.netlify.app/#contact';
+
+    window.location.href = contactUrl;
+})
+
