5629b694f6448cb5db24a100ebfc2d9346b7953f
Shreyas-13-ops <73640884+Shreyas-13-ops@users.noreply.github.com>
2022-01-31 17:28:57 +0530

    Update css

--- script/main.js
+++ script/main.js
@@ -1,10 +1,11 @@
 let btnMenu = document.querySelector(".hamburger");
+let btnSubmit = document.querySelector(".btn-submit");
 let menu = document.querySelector(".menu");
 
 btnMenu.addEventListener('click', () => {
      menu.classList.toggle("active");
-    console.log("btn clciked!");
-})
+    // console.log("Menu btn clicked!");
+});
 
 let btnHello = document.querySelector('.btn-hello');
 let contactSection = document.querySelector('.contact');
@@ -13,5 +14,10 @@ btnHello.addEventListener('click', () => {
    let contactUrl =  'https://dark-theme-portfolio.netlify.app/#contact';
 
     window.location.href = contactUrl;
-})
+});
+
+// btnSubmit.addEventListener('submit', () => {
+//     document.querySelector('form')
+//     console.log("Form btn clicked!");
+// });
 
